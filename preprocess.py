ab4e0e94212010226e97696ab2fbd45834102ab7
Aasees <aaseesbadesha13@gmail.com>
2026-03-21 14:59:22 -0400

    created dataset

--- preprocess.py
+++ preprocess.py
@@ -1,6 +1,8 @@
-from startercode import load_npy, extract_features
-import os
+from startercode import load_npy, reduce_eeg_size, generate_dataset
 from pathlib import Path
+import torch
+
+# TARGET_LENGTH = 128 * 300 # this relates to the time taken to create the dataset
 
 # define file paths
 AD_path = Path('training/AD')
@@ -9,24 +11,48 @@ CN_path = Path('training/CN')
 filesAD = sorted([f for f in AD_path.iterdir() if f.is_file()])
 filesCN = sorted([f for f in CN_path.iterdir() if f.is_file()])
 
-# process data
-def process_data(path):
-    # save npy file id
-    name = path.as_posix().split('/')[2]
-    id = name.split('.')[0]
+# containers
+data_list = []
+labels = []
+subject_ids = []
 
-    # extract features from data
-    data = load_npy(path)
-    rbp, scc = extract_features(data, 128)
-    print(f'rbp {rbp} scc {scc}')
+# load ad -- label 1
+for path in filesAD:
+    data = load_npy(path)  # (channels, time)
 
-def save_data_to_csv(id, rbp, scc):
-    pass
+    id = path.stem  # filename without .npy
 
-# iterate through all files to process
-for path in filesAD:         
-    process_data(path) #TODO: save ad and cn info separately into list or dict or smth
-    break
-# for path in filesCN:
-#     process_data(path)
+    data_list.append(data)
+    labels.append(1)
+    subject_ids.append(id)
 
+# load cn -- label 0
+for path in filesCN:
+    data = load_npy(path)  # (channels, time)
+
+    id = path.stem  # filename without .npy
+
+    data_list.append(data)
+    labels.append(0)
+    subject_ids.append(id)
+
+# # reduce dataset size
+# data_list = reduce_eeg_size(data_list, TARGET_LENGTH)
+
+# generate dataset
+X_rbp, X_scc, y, groups = generate_dataset(
+    data_list,
+    labels,
+    subject_ids,
+    sfreq=128
+)
+
+print('generated')
+
+# save dataset to pt file
+torch.save({
+    "X_rbp": X_rbp,
+    "X_scc": X_scc,
+    "y": y.squeeze(),
+    "groups": groups
+}, "dataset.pt")
